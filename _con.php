09680bec358a1eecae5fad7b3b9ddbf02b9d8664
Bew'Cs <58160083@go.buu.ac.th>
2022-03-02 08:23:20 +0700

    update file connection

--- _con.php
+++ _con.php
@@ -1,7 +1,7 @@
 <?php
 // $conn = new mysqli("127.0.0.1:3308", "admin", "admin999", "dietary");
-$conn = new mysqli("184.168.96.211:3306", "root_999", "sheepcow", "monkshop");
-// $conn = new mysqli("localhost", "root_999", "sheepcow", "monkshop");
+// $conn = new mysqli("184.168.96.211:3306", "root_999", "sheepcow", "monkshop");
+$conn = new mysqli("localhost", "root_999", "sheepcow", "monkshop");
 $conn->set_charset("utf8");
 if ($conn->connect_error) {
     // die("Connection failed: " . $conn->connect_error);
